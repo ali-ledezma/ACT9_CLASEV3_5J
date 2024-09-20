44c5c6f073ff052611551da07a3beac5142324de
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-09-20 09:39:45 -0600

    Add files via upload
    operadores v3

---
+++ ACT9_CLASEV3_OPERADORES_5J/operadoresA.py
@@ -0,0 +1,48 @@
+print("Act 9 clases v2")
+print("Diego Ali Ledezma Carbajal: 22308051281229")
+
+class Operadores1229:
+
+    def suma1229(self,D,L):
+        S1229=D+L
+        print(f"La suma de {D} + {L} = {S1229}")
+        print("--RESTA--")
+    def resta1229(self,D,L):
+        S1229=D-L
+        print(f"La resta de {D} - {L} = {S1229}")
+        print("--DIVICION--")
+    def div1229(self,D,L):
+        S1229=D/L
+        print(f"La divicion de {D} / {L} = {S1229}")
+        print("--MULTIPLICACION--")
+    def multi1229(self,D,L):
+        S1229=D*L
+        print(f"La multiplicacion de {D} * {L} = {S1229}")
+        print("--MODULO--")
+    def mod1229(self,D,L):
+        S1229=D%L
+        print(f"el modulo de {D} % {L} = {S1229}")
+        print("--EXPONENTE--")     
+    def exp1229(self,D,L):
+        S1229=D**L
+        print(f"el exponente de {D} ** {L} = {S1229}")
+        print("--COCIENTE--")
+    def coc1229(self,D,L):
+        S1229=D//L
+        print(f"el cociente de {D} // {L} = {S1229}")
+        print("--MULTIPLICACION--")   
+    
+
+operaali=Operadores1229()
+
+
+print(" Funcion suma")
+operaali.suma1229(5,4)
+operaali.resta1229(5,4)
+operaali.div1229(5,4)
+operaali.multi1229(5,4)
+operaali.mod1229(5,4)
+operaali.exp1229(5,4)
+operaali.coc1229(5,4)
+    
+
